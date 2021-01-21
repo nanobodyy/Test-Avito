953f8a8baafdfec66a9b776e36ed5170620a03b3
Gurgen <gurgenfake@gmail.com>
2021-01-21 20:09:07 +0300

    Edits

--- Test/Choice/Controller/ChoiceViewController.swift
+++ Test/Choice/Controller/ChoiceViewController.swift
@@ -13,22 +13,33 @@ class ChoiceViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var choiceButton: UIButton!
     
-    private var fetchScreen : Screen?
+    private var screens : Screen?
     private var message: String = ""
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        initCollectionView()
+        fetchData()
+    }
+    
+    func initCollectionView() {
         self.collectionView.register(UINib(nibName: "ChoiceCell", bundle: nil), forCellWithReuseIdentifier: "Choice")
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
+    }
+    
+    func updateUI(model: Screen?) {
+        self.collectionView.reloadData()
+        self.infoLabel.text = model?.title
+        self.choiceButton.setTitle(model?.actionTitle, for: .normal)
+    }
+    
+    func fetchData() {
         Network.fetchData { (screen) in
-            self.fetchScreen = screen
-            DispatchQueue.main.async {
-                self.collectionView.reloadData()
-                self.infoLabel.text = self.fetchScreen?.title
-                self.choiceButton.setTitle(self.fetchScreen?.actionTitle, for: .normal)
+                self.screens = screen
+                DispatchQueue.main.async {
+                    self.updateUI(model: self.screens)
+                }
             }
-        }
     }
     
     @IBAction func choiceButton(_ sender: UIButton) {
@@ -44,12 +55,12 @@ extension ChoiceViewController: UICollectionViewDelegate {
         let cell = collectionView.cellForItem(at: indexPath) as! ChoiceCellCollectionViewCell
         if !cell.checkStatus.isHidden {
             cell.checkStatus.isHidden = true
-            self.choiceButton.setTitle(fetchScreen?.actionTitle, for: .normal)
+            self.choiceButton.setTitle(screens?.actionTitle, for: .normal)
             message = "Вы продолжили без изменений"
         } else {
             cell.checkStatus.isHidden = false
-            self.choiceButton.setTitle(fetchScreen?.selectedActionTitle, for: .normal)
-            message = (fetchScreen?.cellItem[indexPath.row].title)!
+            self.choiceButton.setTitle(screens?.selectedActionTitle, for: .normal)
+            message = (screens?.cellItem[indexPath.row].title)!
         }
     }
     
@@ -61,12 +72,12 @@ extension ChoiceViewController: UICollectionViewDelegate {
 
 extension ChoiceViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return fetchScreen?.cellItem.count ?? .zero
+        return screens?.cellItem.count ?? .zero
         }
         
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Choice", for: indexPath) as! ChoiceCellCollectionViewCell
-        cell.configure(screen: fetchScreen!, indexPath: indexPath)
+        cell.configure(screen: screens!, indexPath: indexPath)
             return cell
         }
     
